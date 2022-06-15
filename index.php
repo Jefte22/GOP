b156cbfee06994e7d8c77cfd0a302c311fed02c1
Jefte22 <jerodrigues2208@hotmail.com>
2022-06-15 12:02:28 -0300

    atualizando index

--- index.php
+++ index.php
@@ -1,16 +1,148 @@
 <?php 
-	include "header.php";
-	?>
-		<section id="evento">
-			<div class="row container">
-				<div class="col s6"><span>EVENTO</span>
-					<h2>Sejam<br>Bem-Vindos</h2>
-					<p>Lorem Impsum Dor Sit ametLorem Impsum Dor Sit ametLorem Impsum Dor Sit ametImpsum Dor Sit ametLorem Impsum Dor</p>
-					<a href="" class="inscreva2">INSCREVER-SE</a>
+include "header.php";
+?>
+<section id="evento">
+	<div class="row container">
+		<div class="col s6"><span>EVENTO</span>
+			<h2>Sejam<br>Bem-Vindos</h2>
+			<p>Lorem Impsum Dor Sit ametLorem Impsum Dor Sit ametLorem Impsum Dor Sit ametImpsum Dor Sit ametLorem Impsum Dor</p>
+			<a href="" class="inscreva2">INSCREVER-SE</a>
+		</div>
+		<div class="col s6" id="video"><img src="img/pic.png"><div id="play"><a href=""><img src="img/Play.png"></a></div></div>
+	</div>
+	<div class="row container">
+		<div class="borda">
+			<div id="roundimgs">
+				<div class="col s4 margind"><a href=""><img src="img/data.png"><span>12 jun.</span><span id="sp2">data</span></a>
 				</div>
-				<div class="col s6" id="video"><img src="img/pic.png"><div class="play"><img src="img/Play.png"></div></div>
-			</div>	
-		</section>
-	<?php
-	include "footer.php";
+				<div class="col s4 margind"><a href=""><img src="img/local.png"><span>Guaíra/Pr</span><span id="sp2">local</span></a>
+				</div>
+				<div class="col s4 margind"><a href=""><img src="img/grupo.png"><span>Lorem Ipsum</span><span id="sp2">temas</span></a>
+				</div>
+			</div>
+		</div><!--<div class="polygon2"></div> AJUDA FER-->
+	</div>	
+</section>
+<section>
+	<div class="bgblack">
+		<div class="row container" id="marginb">
+			<div class="col s6"><div class="bitmap">
+				<div class="img1"><a href=""><img src="img/Bitmap1.png"></a></div><div class="polygon3"></div>
+				<div class="img2"><div class="polygon4"></div><div class="cshape"></div><a href=""><img src="img/Bitmap2.png"></a></div>
+			</div><!--imagens no bg-->
+		</div>
+		<div class="col s6">
+			<div class="sobre">
+				<span id="titulo">SOBRE</span>
+				<h2>Sobre <br>o evento</h2>
+				<span class="content">A presença no aniversário de 50 anos da Loja Joaquim Gonçalves Ledo é um exercício de convívio fraternal que complementa a formação de homens íntegros que tem por missão a construção de uma sociedade justa.</span>
+				<div class="content2"></div>
+				<div class="margininsc"><a href="" class="inscreva3">INSCREVER-SE</a></div>
+			</div>
+		</div>
+	</div>
+	<div class="row container">
+		<div class="col s6">
+			<div class="bggray">
+				<span class="Aspas">"</span>
+				<span class="titleG">O QUE FAZER EM GUAÍRA/PR</span>
+				<h2 class="textoG">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</h2>
+				<div class="btnslider"><img src="img/esquerda.png"><img src="img/direita.png">
+					<div class="circulo"></div>
+					<div class="circulo">
+					</div>
+					<div class="circulo active">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col s6">
+			<div class="carouselimg">
+				<img src="img/pordosol.png">
+				<p>Farol<br>
+					<span class="pts">Pontos Turísticos</span>
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="row container">
+		<div class="col s12">
+			<p class="titleG2">SINTA-SE EM CASA</p>
+			<h2 class="rec">Recomendação de Hospedagem</h2>
+		</div>
+	</div>
+	<div class="row container">
+		<div class="col s4">
+			<div class="Fgray">
+				<div class="marginE">
+					<img src="img/papagaio.png">
+				</div>
+				<p>Hotel Papagaio<br>
+					<span class="hts">Hotel</span>
+				</p>
+			</div>
+		</div>
+		<div class="col s4">
+			<div class="Fgray">
+				<div class="marginE">
+					<img src="img/airbnb.png">
+				</div>
+				<p>Hotel Gaúcha<br>
+					<span class="hts">Hotel</span>
+				</p>
+			</div>
+		</div>
+		<div class="col s4">
+			<div class="Fgray">
+				<div class="marginE">
+					<img src="img/hotelgaucha.png">
+				</div>
+				<p>Airbnb<br>
+					<span class="hts">Hotel</span>
+				</p>
+			</div>
+		</div>
+		<div class="col s12">
+			<div class="btnslider2">
+				<img src="img/esquerda.png">
+				<div class="circulo2 active2"></div>
+				<div class="circulo2"></div>
+				<div class="circulo2"></div>
+				<img src="img/direita.png">
+			</div>
+		</div>
+	</div>
+</div>
+</div><!--BACKGROUND - BLACK -->	
+</section>
+<section>
+	<div class="row container">
+		<div class="col s12">
+			<div class="Parh2">
+				<h2>Parceiros</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt</p>
+			</div>
+		</div>
+		<div class="col s3"><div class="parimg">
+			<a href=""><img src="img/google.png"></a>
+		</div>
+	</div>
+	<div class="col s3"><div class="parimg ap">
+		<a href=""><img src="img/apple.png"></a>
+	</div>
+</div>
+<div class="col s3"><div class="parimg">
+	<a href=""><img src="img/par3.png"></a>
+</div>
+</div>
+<div class="col s3"><div class="parimg">
+	<a href=""><img src="img/status.png"></a>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<?php
+include "footer.php";
 ?>
